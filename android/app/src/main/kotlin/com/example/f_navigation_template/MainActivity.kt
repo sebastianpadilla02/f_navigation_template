4a7e718ccbf09b5e0443ced06cf4639dd56d56f0
sebastianpadilla02 <sebastianpadilla@uninorte.edu.co>
2024-08-31 23:01:23 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/f_navigation_template/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.f_navigation_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/ui/my_app.dart
@@ -0,0 +1,43 @@
+import 'package:f_navigation_template/ui/pages/page1.dart';
+import 'package:f_navigation_template/ui/pages/page2.dart';
+import 'package:f_navigation_template/ui/pages/page3a.dart';
+import 'package:f_navigation_template/ui/pages/page3b.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+    @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: FlexThemeData.light(
+        scheme: FlexScheme.greenM3,
+        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+        blendLevel: 7,
+        subThemesData: const FlexSubThemesData(
+          blendOnLevel: 10,
+          blendOnColors: false,
+          useM2StyleDividerInM3: true,
+        ),
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
+        useMaterial3: true,
+        swapLegacyOnMaterial3: true,
+        fontFamily: GoogleFonts.notoSans().fontFamily,
+      ),
+      themeMode: ThemeMode.light,
+      initialRoute: '/page1',
+      getPages: [
+        GetPage(name: '/page1', page: ()=> Page1()),
+        GetPage(name: '/page2', page: ()=> Page2()),
+        GetPage(name: '/page3a', page: ()=> Page3A()),
+        GetPage(name: '/page3b', page: ()=> Page3B()),
+      ],
+      //todo: Implement name routing (initialRoute and getPages parameters)
+    );
+  }
+}
